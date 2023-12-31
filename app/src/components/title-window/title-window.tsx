866bb09119a87ab6379b16747609dfa6a686e551
Chirag <chirag.jain1@bfhl.in>
2024-01-01 03:52:56 +0530

    searchIcon styled component

--- app/src/components/title-window/title-window.tsx
+++ app/src/components/title-window/title-window.tsx
@@ -1,5 +1,4 @@
 import css from "./title-window.module.css";
-import appCss from "@/components/app/app.module.css";
 
 import { SearchIcon } from "@codepp/ui/icons";
 
@@ -13,9 +12,7 @@ export default function TitleWindow({ title, windowActive }: TitleWindowProps) {
     <div
       className={`${css["title-window"]} ${!windowActive ? css.inactive : ""}`}
     >
-      <SearchIcon
-        className={`${appCss["search-icon"]} ${css["search-icon"]}`}
-      />
+      <SearchIcon className={css["search-icon"]} />
       {title}
     </div>
   );
--- packages/codepp-ui/src/icons/index.ts
+++ packages/codepp-ui/src/icons/index.ts
@@ -1,3 +1,12 @@
+import { default as MuiSearchIcon } from "@mui/icons-material/Search";
+import { styled } from "@mui/material";
+
 export { default as MenuSharpIcon } from "@mui/icons-material/MenuSharp";
 export { default as FileCopyOutlinedIcon } from "@mui/icons-material/FileCopyOutlined";
-export { default as SearchIcon } from "@mui/icons-material/Search";
+
+export const SearchIcon = styled(MuiSearchIcon)<{ transform?: string }>`
+  transform: ${({ transform }) =>
+    transform
+      ? `${transform} ${!transform.includes("rotate") ? "rotate(90deg)" : ""}`
+      : "rotate(90deg)"};
+`;
