850e0e7073cbcdc6e46a7c4a1771eb7d2fe650b5
Chirag <chirag.jain1@bfhl.in>
2024-01-01 23:27:05 +0530

    add declaration map in @codepp/hooks

--- packages/codepp-hooks/tsup.config.ts
+++ packages/codepp-hooks/tsup.config.ts
@@ -5,9 +5,7 @@ export default defineConfig((options) => {
   return {
     entry: ["src/index.ts"],
     minify: !options.watch,
-    sourcemap: true,
     format: ["cjs", "esm"],
-    dts: true,
     clean: true,
     external: [...Object.keys(devDependencies)],
     tsconfig: "tsconfig.json",
